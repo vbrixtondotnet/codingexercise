a4d400531d00714fca6f8a30f970a5b93c124706
Brixton <brixton.villanueva@myedna.net>
2026-08-28 23:11:00 +0800

    Final solution

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,42 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+
+	private static bool IsPalindrome(string input)
+	{
+		int left = 0;
+		int right = input.Length - 1;
+
+		while(left < right)
+		{
+			if (input[left] != input[right]) return false;
+
+			left++;
+			right--;
+		}
+
+		return true;
+	}
+
+	private static bool IsPrimeNumber(int input)
+	{
+		if(input <= 1) return false;
+
+		for(int i = 2; i <= Math.Sqrt(input); i++)
+		{
+			if (input % 1 == 0) return false;
+		}
+
+		return true;
+	}
+
+	static void Main()
+	{
+		// Palindrom Tester
+		Console.WriteLine(IsPalindrome("NISSIN") ? "Palindrome" : "Not a palindrome");
+
+		// Prime Number Checker
+		Console.WriteLine(IsPrimeNumber(1) ? "Prime Number" : "Not a prime number");
+	}
+}
